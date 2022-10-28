d6dde53e32b898f955b49125568394e672f164bb
Wes Cook <im@wescook.ca>
2022-10-28 12:52:31 -0700

    Creating settings and assets

---
+++ src/setup.mjs
@@ -0,0 +1,29 @@
+export function setup({ settings, onInterfaceReady }) {
+	// Settings
+	createSettings(settings);
+}
+
+function createSettings(settings) {
+	const sectionGeneral = settings.section("General");
+
+	// Duration
+	sectionGeneral.add({
+		type: "dropdown",
+		name: "duration",
+		label: "Duration",
+		hint: " ​ ​ Auto-dismiss popups within this duration", // Minor hack: using zero-width space characters to create margin
+		default: "10s",
+		onChange: newValue => {
+			// TODO: Implement settings callback
+		},
+		options: [
+			{ value: "disable", display: "0 seconds (Disable)" },
+			{ value: "1s", display: "1 second" },
+			{ value: "10s", display: "10 seconds" },
+			{ value: "1m", display: "1 minute" },
+			{ value: "10m", display: "10 minutes" },
+			{ value: "1h", display: "1 hour" },
+			{ value: "forever", display: "Forever" }
+		]
+	});
+}
